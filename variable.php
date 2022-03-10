3a4acecf9bd645047be6b04700ac889b1e40a265
Tegar11-F <tegarfakhruddin12@gmail.com>
2022-03-10 11:46:21 +0700

    Upload variable.php

---
+++ variable.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>variable</title>
+</head>
+<body>
+    <?php
+    $x = 2;
+    $y = 3;
+    $hasilJumlah = $x+$y;
+    $hasilKurang = $x-$y;
+    $hasilKali = $x*$y;
+    echo "Hasil penjulahan dari : ".$x."+".$y."=".$hasilJumlah.'<br>';
+    echo "Hasil pengurangan dari : ".$x."-".$y."=".$hasilKurang.'<br>';
+    echo "Hasil penjulahan dari : ".$x."*".$y."=".$hasilKali;
+        ?>
+</body>
+</html>
