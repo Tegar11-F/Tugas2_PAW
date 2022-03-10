2d77f4a1f9a6b39205a63999abdfde406e0cca1e
Tegar11-F <tegarfakhruddin12@gmail.com>
2022-03-10 11:46:42 +0700

    Upload fungsi.php

---
+++ fungsi.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>hello</title>
+</head>
+<body>
+<?php
+function a() {
+  echo "Hello world! <br>";
+}
+
+a();
+a();
+a();
+?>
+</body>
+</html>
