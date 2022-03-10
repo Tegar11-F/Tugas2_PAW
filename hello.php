cffaaf58d316d7ccf08de542d058624fe7db2fc9
Tegar11-F <tegarfakhruddin12@gmail.com>
2022-03-10 11:45:44 +0700

    Upload hello.php

---
+++ hello.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>hello</title>
+</head>
+<body>
+    <?php
+    echo "Nama  : Tegar Fakhruddin <br> Nim : 200411100124" 
+    ?>
+</body>
+</html>
